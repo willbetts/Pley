dcf07d22d3f003b5499baa92415ea1e77630533e
willbetts <w.j.betts@gmail.com>
2017-02-16 11:31:19 -0500

    successfully seeded a tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create(
-#   first_name: "guest",
-#   last_name: "guest",
-#   email: "guest@guest.com",
-#   password: "starwars",
-#   birthday: "",
-#   session_token: "1111111111111111"
-#   )
+User.create(
+  first_name: "guest",
+  last_name: "guest",
+  email: "guest@guest.com",
+  password: "starwars",
+  birthday: "",
+  session_token: "1111111111111111"
+  )
 
 Business.create(
   name: "Peter Lugers",
@@ -25,4 +25,8 @@ Business.create(
   hours: {monday: "1-1"},
   price: 4,
   pickup_delivery: false
+)
+
+Tag.create(
+  name: "steak"
 )
