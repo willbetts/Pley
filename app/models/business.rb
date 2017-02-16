edebd1a14d9dcc88d8c47b0e0044fce423881b4d
willbetts <w.j.betts@gmail.com>
2017-02-16 11:27:08 -0500

    business controller than can successfully create businesses

--- app/models/business.rb
+++ app/models/business.rb
@@ -4,7 +4,7 @@
 #
 #  id              :integer          not null, primary key
 #  name            :string           not null
-#  type            :string           not null
+#  business_type   :string           not null
 #  image_url       :string           not null
 #  address         :string           not null
 #  phone_number    :string           not null
@@ -15,7 +15,7 @@
 #
 
 class Business < ActiveRecord::Base
-  validates :name, :type, :image_url, :address, :phone_number, :hours, :price, presence: true;
+  validates :name, :business_type, :image_url, :address, :phone_number, :hours, :price, presence: true;
   has_many :taggings
 
 end
