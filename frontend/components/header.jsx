93c0eeb53eafb4e39732f550083401b8a75b3850
Will Betts <willbetts@Wills-MBP.home>
2017-02-16 01:15:01 -0500

    near complete auth css styling

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -4,12 +4,24 @@ import { Link } from 'react-router'
 const Header = (props) => {
 
   if (props.currentUser) {
-    return (<button className="header-button" id="logout" onClick={props.logout}>Log Out</button>)
+    return (
+      <div className="log-out-button">
+        <button id="logout" onClick={props.logout}>Log Out</button>
+      </div>
+    )
   } else {
       return(
           <ul className="login-buttons">
             <Link className="hdrbtn" id="login"  to={"/login"}>Log In</Link>
             <Link className="hdrbtn" id="signup" to={"/signup"}>Sign Up </Link>
+            <a className="hdrbtn" id="guest" onClick={() => {
+                props.login({
+                  email: "guest@guest.com",
+                  password: "starwars"
+                })
+              }}>
+              Guest Log In
+            </a>
           </ul>
     );
   }
--- frontend/components/header_container.jsx
+++ frontend/components/header_container.jsx
@@ -1,13 +1,14 @@
 import { connect } from 'react-redux';
 import Header from './header';
-import { logout } from '../actions/session_actions';
+import { login, logout } from '../actions/session_actions';
 
 const mapStateToProps = ({ session }) => ({
   currentUser: session.currentUser
 });
 
 const mapDispatchToProps = dispatch => ({
-  logout: () => dispatch(logout())
+  logout: () => dispatch(logout()),
+  login: (user) => dispatch(login(user))
 });
 
 export default connect(
