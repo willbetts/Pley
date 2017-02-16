0a5393e2d188ba793dea9ab312222465cf5ad875
willbetts <w.j.betts@gmail.com>
2017-02-16 10:28:14 -0500

    added models: business, tag, tagging

---
+++ app/models/tagging.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: taggings
+#
+#  id          :integer          not null, primary key
+#  business_id :integer          not null
+#  tag_id      :integer          not null
+#
+
+class Tagging < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :tag
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  birthday        :string
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
   attr_reader :password
 
