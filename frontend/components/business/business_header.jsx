7a7ca6d44e7c00c19d72d57dca261bcd3dfa2caf
Will Betts <willbetts@Wills-MacBook-Pro.local>
2017-02-18 15:46:47 -0500

    finished styling header for search results

--- frontend/components/business/business_header.jsx
+++ frontend/components/business/business_header.jsx
@@ -4,16 +4,16 @@ import { Link } from 'react-router'
 const BusinessHeader = (props) => {
   if (props.currentUser) {
     return (
-      <div className="business-log-out-button">
-        <button id="logout" onClick={props.logout}>Log Out</button>
+      <div>
+        <button id="business-logout" className="business-hdrbtn" onClick={props.logout}>Log Out</button>
       </div>
     );
   } else {
       return(
           <ul className="business-login-buttons">
-            <Link className="business-hdrbtn" id="login" onClick={props.resetErrors}  to={"/login"}>Log In</Link>
-            <Link className="business-hdrbtn" id="signup" onClick={props.resetErrors} to={"/signup"}>Sign Up </Link>
-            <a className="business-hdrbtn" id="guest" onClick={() => {
+            <Link className="business-hdrbtn" onClick={props.resetErrors}  to={"/login"}>Log In</Link>
+            <Link className="business-hdrbtn" onClick={props.resetErrors} to={"/signup"}>Sign Up </Link>
+            <a className="business-hdrbtn" onClick={() => {
                 props.login({
                   email: "guest@guest.com",
                   password: "starwars"
