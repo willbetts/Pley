067cc174ae665a9654df64ed0cf789fe4072d106
willbetts <w.j.betts@gmail.com>
2017-02-26 16:37:37 -0500

    fixed image

--- app/models/business.rb
+++ app/models/business.rb
@@ -19,7 +19,7 @@ class Business < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings, source: :tag
   has_many :reviews
-  has_attached_file :image,  default_url: "/assets/images/steak.jpg"
+  has_attached_file :image,  default_url: "/assets/steak.jpg"
 
   def self.search(query)
     Business
